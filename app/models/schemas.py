e4ae7879037d35f9d89f621bd6af711ab712843e
jeff-901 <b07901052@ntu.edu.tw>
2025-05-17 15:14:49 -0700

    update

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -11,26 +11,86 @@ class Location(BaseModel):
     longitude: float
 
 
-class ProviderSpecialty(BaseModel):
-    """Provider specialty information."""
+class Specialty(BaseModel):
+    """Detailed specialty information."""
     id: str
-    name: str
+    specialty_name: str = Field(..., alias="specialtyName")
+    specialty_id: Optional[str] = Field(None, alias="specialtyId")
     description: Optional[str] = None
+    exclude: bool = False
+    physician_specialty: Optional[Dict] = Field(None, alias="physicianSpecialty")
+
+
+class BoardCertification(BaseModel):
+    """Board certification information."""
+    name: str
+    source: str
+
+
+class BoardCertifications(BaseModel):
+    """Collection of board certifications."""
+    board_certifications: List[BoardCertification] = Field(..., alias="boardCertifications")
+
+
+class Physician(BaseModel):
+    """Detailed physician information."""
+    name: str
+    date_imported: str = Field(..., alias="dateImported")
+    hash: str
+    id: str
+    provider_id: str = Field(..., alias="providerId")
+    address_line1: str = Field(..., alias="addressLine1")
+    address_city: str = Field(..., alias="addressCity")
+    address_state: str = Field(..., alias="addressState")
+    address_zipcode: str = Field(..., alias="addressZipcode")
+    phone: Optional[str] = None
+    geocode_source: str = Field(..., alias="geocodeSource")
+    first_name: str = Field(..., alias="firstName")
+    last_name: str = Field(..., alias="lastName")
+    middle_name: Optional[str] = Field(None, alias="middleName")
+    suffix: Optional[str] = None
+    gender: str
+    credential: str
+    medical_school_name: str = Field(..., alias="medicalSchoolName")
+    graduation_year: str = Field(..., alias="graduationYear")
+    telehealth: bool
+    organization_name: Optional[str] = Field(None, alias="organizationName")
+    pac_id: str = Field(..., alias="pacId")
+    group_practice_pac_id: Optional[str] = Field(None, alias="groupPracticePacId")
+    group_accepts_medicare_assignment: bool = Field(..., alias="groupAcceptsMedicareAssignment")
+    address_line2: Optional[str] = Field(None, alias="addressLine2")
+    address_id: str = Field(..., alias="addressId")
+    address_line2_suppress: bool = Field(..., alias="addressLine2suppress")
+    accepts_medicare_assignment: str = Field(..., alias="acceptsMedicareAssignment")
+    has_measures: bool = Field(..., alias="hasMeasures")
+    has_facility_measures: bool = Field(..., alias="hasFacilityMeasures")
+    has_updated_phone: bool = Field(..., alias="hasUpdatedPhone")
+    is_duplicate_address: bool = Field(..., alias="isDuplicateAddress")
+    is_canonical_address: bool = Field(..., alias="isCanonicalAddress")
+    specialties: List[Specialty]
+    group_affiliations: List = Field(..., alias="groupAffiliations")
+    board_certifications: BoardCertifications
+    class Config:
+        extra = "allow"
 
 
 class ProviderInfo(BaseModel):
     """Provider information."""
-    id: str
+    distance: float
     name: str
-    specialties: List[ProviderSpecialty]
-    address: str
-    zip_code: str
-    location: Location
-    distance: Optional[float] = None
-    phone: Optional[str] = None
-    website: Optional[str] = None
-    rating: Optional[float] = None
-    availability: Optional[Dict] = None
+    id: str
+    type: str
+    provider_id: str = Field(..., alias="providerId")
+    sort_name: str = Field(..., alias="sortName")
+    first_name: str = Field(..., alias="firstName")
+    last_name: str = Field(..., alias="lastName")
+    address_state: str = Field(..., alias="addressState")
+    is_duplicate_address: bool = Field(..., alias="isDuplicateAddress")
+    is_canonical_address: bool = Field(..., alias="isCanonicalAddress")
+    sort_name_alt: str = Field(..., alias="sort_name")
+    physician: Physician
+    class Config:
+        extra = "allow"
 
 
 class ProviderConfirmationInfo(BaseModel):
